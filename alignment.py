10dc03b4af3fb09d35cf8498377810c76bc93df4
Jonathan Hull <j.hull2@newcastle.ac.uk>
2018-02-17 05:23:29 +0000

    Created initialisation / constructor / boolmatrix

---
+++ alignment.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python2.7
+
+import sys
+import numpy as np
+
+seqA = "AGTAAACCGTA"
+seqB = "AGTAACGTA"
+
+class LocalAlignment:
+
+    def __init__(self, seq1, seq2, seq_match=3, seq_miss=-3, seq_del=-2):
+
+        self.seq1 = seq1
+        self.seq1L = list(seq1)
+        self.seq2L = list(seq2)
+        self.seq2 = seq2
+        self.seq_match = seq_match
+        self.seq_miss = seq_miss
+        self.seq_del = seq_del
+
+    def start(self):
+        """Initialise local sequence alignment algorithms"""
+
+        self.boolMat = self.boolMatrixGen()
+
+
+    def baseMatrix(self):
+        """Generates initial base numpy matrix"""
+        return np.zeros([len(self.seq1)+1, len(self.seq2)+1])
+
+
+    def boolMatrixGen(self):
+        """Generates boolean similarity matrix from nucleotide sequences"""
+
+        matrix = np.zeros([len(self.seq1), len(self.seq2)], dtype=bool)
+
+        seq1L = list(self.seq1)
+        seq2L = list(self.seq2)
+
+        for r in range(len(seq1L)):
+            for c in range(len(seq2L)):
+                if seq1L[r] == seq2L[c]:
+                    matrix[r][c] = True
+
+        return matrix
+
+
+    def scoreMatrix(self):
+        """Generates the score matrix from the base matrix"""
+
+        matrix = np.zeros([len(self.seq1)+1, len(self.seq2)+1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = LocalAlignment(seqA, seqB)
+x.start()
+print(x.boolMat)
