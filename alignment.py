99c4b75de62cc81bcaf5c866640aa12b02fa8117
Jonathan Hull <Jonathan.hull11@gmail.com>
2018-02-17 08:37:10 +0000

    Incorporated pathing of sequence scoring matrix.

--- alignment.py
+++ alignment.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 
 import sys
+import random
 import numpy as np
 
 seqA = "AGTAAACCGTA"
@@ -11,8 +12,6 @@ class LocalAlignment:
     def __init__(self, seq1, seq2, seq_match=3, seq_miss=-3, seq_del=-2):
 
         self.seq1 = seq1
-        self.seq1L = list(seq1)
-        self.seq2L = list(seq2)
         self.seq2 = seq2
         self.seq_match = seq_match
         self.seq_miss = seq_miss
@@ -22,13 +21,9 @@ class LocalAlignment:
         """Initialise local sequence alignment algorithms"""
 
         self.boolMat = self.boolMatrixGen()
-
         self.scoreMat = self.scoreMatrix(self.boolMat)
 
-
-    def baseMatrix(self):
-        """Generates initial base numpy matrix"""
-        return np.zeros([len(self.seq1)+1, len(self.seq2)+1])
+        self._pathMatrix(self.scoreMat)
 
 
     def boolMatrixGen(self):
@@ -41,7 +36,7 @@ class LocalAlignment:
 
         for r in range(len(seq1L)):
             for c in range(len(seq2L)):
-                if self.seq1L[r] == self.seq2L[c]:
+                if seq1L[r] == seq2L[c]:
                     matrix[r][c] = True
 
         return matrix
@@ -69,15 +64,51 @@ class LocalAlignment:
         return matrix
 
 
+    def _pathMatrix(self, matrix):
+        """Determines path through sequence scoring matricies."""
 
+        seq1L = list(self.seq1)
+        seq2L = list(self.seq2)
+        outList = []
 
+        r, c = np.unravel_index(matrix.argmax(), matrix.shape)
 
+        while r and c != 0:
+            diag = matrix[r-1][c-1]
+            horizontal = matrix[r][c-1]
+            vertical = matrix[r-1][c]
 
+            g = max(diag, horizontal, vertical)
 
+            if g == diag:
+                outChar = (seq1L[r-1], seq2L[c-1])
+                r -= 1
+                c -= 1
 
+            elif horizontal == vertical:
+                if random.random() < 0.5:
+                    outChar = (seq1L[r-1], "-")
+                    r -= 1
 
+                else:
+                    outChar = ("-", seq2L[c-1])
+                    c -= 1
+
+            elif g == horizontal:
+                outChar = ("-", seq2L[c-1])
+                c -= 1
+
+            else:
+                outChar = (seq1L[r-1], "-")
+                r -= 1
+
+            outList.append(outChar)
+
+        return outList
+
+    def _seqAssembly(self, seqList):
+        """Builds sequence alignment from path prediction."""
 
 
 x = LocalAlignment(seqA, seqB)
 x.start()
-print(x.scoreMat)
